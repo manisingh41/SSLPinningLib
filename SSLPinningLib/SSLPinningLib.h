dfbbcfb401bffbd76e483bc992c8e6f21dbece89
Nagmani Singh <nagmani.singh@zensar.com>
2022-07-26 15:41:43 +0530

    initial

---
+++ SSLPinningLib/SSLPinningLib.h
@@ -0,0 +1,18 @@
+//
+//  SSLPinningLib.h
+//  SSLPinningLib
+//
+//  Created by Nagmani Singh on 26/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SSLPinningLib.
+FOUNDATION_EXPORT double SSLPinningLibVersionNumber;
+
+//! Project version string for SSLPinningLib.
+FOUNDATION_EXPORT const unsigned char SSLPinningLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSLPinningLib/PublicHeader.h>
+
+
